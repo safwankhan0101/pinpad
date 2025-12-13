93e271e8969ddaaf0256f77bf129bbefc97f03f9
Safwan khan <safwankhan010101@gamil.com>
2025-12-13 20:27:16 +0500

    Initial Commit

---
+++ pinpad/pinpadApp.swift
@@ -0,0 +1,17 @@
+//
+//  pinpadApp.swift
+//  pinpad
+//
+//  Created by Apple Orchard on 13/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct pinpadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ pinpadTests/pinpadTests.swift
@@ -0,0 +1,17 @@
+//
+//  pinpadTests.swift
+//  pinpadTests
+//
+//  Created by Apple Orchard on 13/12/2025.
+//
+
+import Testing
+@testable import pinpad
+
+struct pinpadTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

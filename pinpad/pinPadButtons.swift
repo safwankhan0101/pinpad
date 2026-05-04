5bab6eed4e1544df9c50e1e1498a425e94de400a
Safwan khan <safwankhan010101@gamil.com>
2026-05-04 23:20:00 +0500

    first commit

--- pinpad/pinPadButtons.swift
+++ pinpad/pinPadButtons.swift
@@ -1,18 +1,39 @@
 //
-//  pinPadButtons.swift
-//  pinpad
+//  PinPadButtons.swift
+//  PinButtons
 //
-//  Created by Apple Orchard on 13/12/2025.
+//  Created by Iqbal Alhadad on 13/12/25.
 //
 
 import SwiftUI
 
-struct pinPadButtons: View {
+struct PinPadButtons: View {
+    var number: String? = nil
+    var icon:String? = nil
+    var action: () -> Void
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button(action: action){
+            if let number = number {
+                Text(number)
+                    .font(.title)
+                    .frame(width: 80, height: 80)
+                    .foregroundStyle(Color.black)
+                    .background(Circle().fill(Color.black.opacity(0.15)))
+            }
+            else if let icon = icon {
+                Image(systemName: icon)
+                    .font(.title)
+                    .frame(width: 80, height: 80)
+                    .foregroundStyle(Color.black)
+                    .background(Circle().fill(Color.black.opacity(0.15)))
+            }
+        }
     }
 }
 
 #Preview {
-    pinPadButtons()
+    PinPadButtons(number: nil, icon: "person") {
+        debugPrint("Pressed")
+    }
 }
